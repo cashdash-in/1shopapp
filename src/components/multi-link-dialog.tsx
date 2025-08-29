c297269c8085fb14d6432dab25468d7dfbc2b7a6
Christopher D'souza <christopher.p.dsouza@gmail.com>
2025-08-29 19:28:30 +0000

    Give another colour to the dialogue box opening after clicking tiles

--- src/components/multi-link-dialog.tsx
+++ src/components/multi-link-dialog.tsx
@@ -23,7 +23,7 @@ export function MultiLinkDialog({ service, children }: MultiLinkDialogProps) {
   return (
     <Dialog>
       <DialogTrigger asChild>{children}</DialogTrigger>
-      <DialogContent className="sm:max-w-[425px]">
+      <DialogContent className="sm:max-w-[425px] bg-secondary">
         <DialogHeader>
           <div className="flex items-center gap-3 mb-4">
              <Icon className="w-8 h-8" style={{ color: service.color }}/>
