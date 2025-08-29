8174513a063c277e762fb080d3cae68067625806
Christopher D'souza <christopher.p.dsouza@gmail.com>
2025-08-29 19:29:03 +0000

    Give some bright color

--- src/components/multi-link-dialog.tsx
+++ src/components/multi-link-dialog.tsx
@@ -23,7 +23,7 @@ export function MultiLinkDialog({ service, children }: MultiLinkDialogProps) {
   return (
     <Dialog>
       <DialogTrigger asChild>{children}</DialogTrigger>
-      <DialogContent className="sm:max-w-[425px] bg-secondary">
+      <DialogContent className="sm:max-w-[425px] bg-background">
         <DialogHeader>
           <div className="flex items-center gap-3 mb-4">
              <Icon className="w-8 h-8" style={{ color: service.color }}/>
@@ -35,8 +35,8 @@ export function MultiLinkDialog({ service, children }: MultiLinkDialogProps) {
             <Button
               key={link.name}
               asChild
-              variant="outline"
-              className="justify-between h-12"
+              variant="secondary"
+              className="justify-between h-12 text-base"
             >
               <Link
                 href={link.href}
